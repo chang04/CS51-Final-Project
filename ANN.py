38c46fe129a165de8f53f005d7a858039a917da8
zachbai <zachrybai@gmail.com>
2015-04-30 18:54:17 -0400

    done with deltas

--- ANN.py
+++ ANN.py
@@ -26,7 +26,7 @@ class NeuralNetwork:
         return y*(y-1)
 
 
-    def Run_one(self, input):
+    def run(self, input):
         self.layerIn = []
         self.layerOut = []
 
@@ -36,19 +36,29 @@ class NeuralNetwork:
             if i == 0:
                 layer = self.weights[0].dot(np.vstack((input.transpose(), np.ones([1,dim]))))
             else:
-                layer = self.weights[i].dot(np.vstack((self.layerOut[-1], np.ones(1))))
+                layer = self.weights[i].dot(np.vstack((self.layerOut[-1], np.ones([1,dim]))))
             self.layerIn.append(layer)
             self.layerOut.append(self.sgm(layer))
 
         return (self.layerOut[-1]).transpose()
 
-    def Run_multiple(self, input):
+    def delta(self, input, y):
+        output = []
+        y_hat = self.run(input)
+
+        for i in reversed(range(self.layers)):
+            if i == self.layers-1:
+                error = y_hat - y
+                output.append(self.dersgm(y_hat) * error)
+            else:
+                error = self.weights[i+1][:, :-1].dot(output[-1])
+                output.append(self.dersgm(self.layerOut[i]) * error)
+        return output
+
+    def correct_weights(self, deltas, learning_rate):
 
 
 
 bpn = NeuralNetwork([2,2,1])
-input = np.array([[1,2]])
-print bpn.Run_Bitch(input)
-
-
-
+input = np.array([[1,2],[3,4]])
+output = np.array([2,5])
