b38582d4d99e0ed959d872dbc1ed98a0baa0bb50
zachbai <zachrybai@gmail.com>
2015-05-01 15:08:08 -0400

    gradient descent for large input doesnt work

--- Neural/ANN.py
+++ Neural/ANN.py
@@ -16,14 +16,19 @@ class NeuralNetwork:
         for i in range(0,self.layers):
             n = shape[i]
             m = shape[i+1]
-            self.weights.append(np.random.normal(scale=1 , size = (m,n+1)))
+            self.weights.append(np.random.normal(scale=0.01, size = (m,n+1)))
 
     def sgm(self, x):
         return 1/(1+np.exp(-x))
 
+    def dertanh(self, x):
+        out = np.tanh(x)
+        return 1 - out ** 2
+
+
     def dersgm(self, x):
         y = self.sgm(x)
-        return y*(y-1)
+        return y*(1-y)
 
 
     def run(self, input):
@@ -36,7 +41,7 @@ class NeuralNetwork:
             else:
                 layer = self.weights[i].dot(np.vstack((self.layerOut[-1], np.ones([1,input.shape[0]]))))
             self.layerIn.append(layer)
-            self.layerOut.append(self.sgm(layer))
+            self.layerOut.append(np.tanh(layer))
 
         return self.layerOut[-1].T
 
@@ -50,17 +55,19 @@ class NeuralNetwork:
 
             #for last layer
             if i == self.layers-1:
-                error = y_hat - y
-                msq_error = sum(.5 * ((error) ** 2))
-                #returns delta, k rows for k inputs, m columns for m nodes
-                deltas.append(error * self.dersgm(y_hat))
+                out_delt = self.layerOut[i] - y.T
+                msq_error = np.sum(.5 * ((out_delt) ** 2))
+                #returns delta, k columns for k inputs, m rows for m nodes
+                deltas.append(out_delt * self.dertanh(self.layerIn[i]))
             else:
-                error = deltas[-1].dot(self.weights[i+1][:,:-1])
-                deltas.append(self.dersgm(self.layerOut[i]).T * error)
+                #backprop -- uses subsequents layer delta to calculate next delta
+                last_deltas = self.weights[i+1].T.dot(deltas[-1])
+                deltas.append(last_deltas[:-1,:] * self.dertanh(self.layerIn[i]))
 
         #Calculate weight-deltas
         wdelta = []
         ordered_deltas = list(reversed(deltas)) #reverse order because created backwards
+
 
         #returns weight deltas, k rows for k nodes, m columns for m next layer nodes
         for i in range(self.layers):
@@ -69,19 +76,16 @@ class NeuralNetwork:
                 input_with_bias = np.vstack((input.T, np.ones(input.shape[0])))
                 #some over n rows of deltas for n training examples to get one delta for all examples
                 #for all nodes
-                wdelta.append(ordered_deltas[i].T.dot(input_with_bias.T))
+                wdelta.append(ordered_deltas[i].dot(input_with_bias.T))
             else:
                 with_bias = np.vstack((self.layerOut[i-1], np.ones(input.shape[0])))
-                wdelta.append(ordered_deltas[i].T.dot(with_bias.T))
-
-
+                wdelta.append(ordered_deltas[i].dot(with_bias.T))
 
         #update_weights
         def update_weights(self, weight_deltas, learning_rate):
             for i in range(self.layers):
-                self.weights[i] = self.weights[i] +\
+                self.weights[i] = self.weights[i] -\
                                   (learning_rate * weight_deltas[i])
-
 
         update_weights(self, wdelta, learning_rate)
 
@@ -91,8 +95,12 @@ class NeuralNetwork:
 
     def train(self, input, target, lr, run_iter):
         for i in range(run_iter):
-            if i % 100 == 0:
-                print self.backpropogate(input, target, lr)
+            a = self.backpropogate(input, target, lr)
+            if i % 5000 == 0:
+                print "Training..."
+            if run_iter - i == 1: print a
+
+
 
 
 
--- Neural/main.py
+++ Neural/main.py
@@ -7,25 +7,27 @@ import ANN
 reader=csv.reader(open("1500movies_with_gross.csv","rb"),delimiter=',')
 input = list(reader)
 input = np.array(input)
-outputs = input[1:,[6]]
+outputs = input[1:,[5]]
 outputs = np.array(outputs, np.float)
-input = input[1:,[1,2,3,4,5]]
+input = input[1:,[1,2,3,4,6]]
 input = np.array(input, np.float)
 
-
+"""
+#Sample Network
 bpn = ANN.NeuralNetwork([2,2,1])
 y = np.array([[1,2],[3,4],[9,1]])
 x = np.array([[1,2]])
+zeros = np.array([0])
 z = np.array([[0,0],[1,1],[0,1],[1,0]])
-target = np.array([[0.05],[0.05],[0.95],[0.95]])
+target = np.array([[0.05],[0.05],[0.7]])
 output = np.array([[6]])
 
-bpn.train(z,target,.2,1)
 
+bpn.train(y, target,0.2, 10000)
+"""
 
+NeuralNetwork = ANN.NeuralNetwork([5,4,3,1])
+NeuralNetwork.train(input, outputs, 0.2, 50000)
+print NeuralNetwork.run(np.array([[50000000, 20, 4, 112, 191719337]]))
 
-
-
-NeuralNetwork = ANN.NeuralNetwork([5,5,1])
-NeuralNetwork.train(input, outputs, .01, 100000)
 
