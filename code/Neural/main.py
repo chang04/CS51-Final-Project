8bdaeb86e8a1d5c2ae76ba43b625e98d97aec98e
zachbai <zachrybai@gmail.com>
2015-05-01 17:07:29 -0400

    final

--- code/Neural/main.py
+++ code/Neural/main.py
@@ -13,30 +13,31 @@ input = input[1:,[1,2,3,4,6]]
 input = np.array(input, np.float)
 
 
-""""
+
 
 #Sample Network
 
 bpn = ANN.NeuralNetwork([2,2,1])
-y = np.array([[1,2],[3,4],[9,1]])
+input = np.array([[1,2],[3,4],[9,1]])
 x = np.array([[1,2]])
 zeros = np.array([0])
 z = np.array([[0,0],[1,1],[0,1],[1,0]])
 target = np.array([[2],[2],[0.7]])
 output = np.array([[6]])
 
-
-
-bpn.train(y, target,0.2, 10000)
+bpn.train(input, target,0.2, 10000)
 
 print bpn.run(np.array([[3,4]]))
 
 
 
 """
+#converges to nearly 0, but not fully functional.
+
 NeuralNetwork = ANN.NeuralNetwork([5,4,3,1])
 NeuralNetwork.train(input, outputs, 0.0007, 50000)
 print NeuralNetwork.run(np.array([[50000000, 20, 4, 112, 191719337]]))
 print NeuralNetwork.run(np.array([[8000000, 5, 3, 90, 2763020]]))
 print NeuralNetwork.run(np.array([[100000000, 22, 4, 165, 162805434]]))
 
+"""
